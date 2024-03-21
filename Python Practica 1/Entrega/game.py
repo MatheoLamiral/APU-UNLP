1913470443dd3ea3dfa64735bd9766283413532e
Matheo Joaquín Lamiral <matheolamiral05@gmail.com>
2024-03-21 11:46:51 -0300

    solucion a bug del enter en game.py

--- Python Practica 1/Entrega/game.py
+++ Python Practica 1/Entrega/game.py
@@ -25,10 +25,9 @@ for i in range(max_attempts):
         print("Ya has intentado con esa letra. Intenta con otra.")
         continue
     # Agregar la letra a la lista de letras adivinadas
-
     guessed_letters.append(letter)
-    # Verificar si la letra está en la palabra secreta
-    if letter in secret_word:
+    # Verificar si la letra está en la palabra secreta y es distinto del string "vacio"
+    if letter in secret_word and letter != "":
         print("¡Bien hecho! La letra está en la palabra.")
     else:
         print("Lo siento, la letra no está en la palabra.")
