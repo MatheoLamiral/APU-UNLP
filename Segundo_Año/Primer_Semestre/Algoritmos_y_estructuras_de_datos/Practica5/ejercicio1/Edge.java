c87a397ead62bc121d73873eaca0c5403eb45fa3
Matheo Joaquín Lamiral <matheolamiral05@gmail.com>
2024-06-25 19:58:17 -0300

    Practica 5 AYED (ejercicio 1)

---
+++ Segundo_Año/Primer_Semestre/Algoritmos_y_estructuras_de_datos/Practica5/ejercicio1/Edge.java
@@ -0,0 +1,19 @@
+package Practica5.ejercicio1;
+
+public interface Edge<T> {
+	
+	/**
+	 * Retorna el vértice destino de la arista.
+	 * 
+	 * @return
+	 */
+	public Vertex<T> getTarget();
+	
+	/**
+	 * Retorna el peso de la arista
+	 * 
+	 * @return
+	 */
+	public int getWeight();
+
+}
---
+++ Segundo_Año/Primer_Semestre/Algoritmos_y_estructuras_de_datos/Practica5/ejercicio1/adjList/AdjListEdge.java
@@ -0,0 +1,28 @@
+package Practica5.ejercicio1.adjList;
+
+import Practica5.ejercicio1.Edge;
+import Practica5.ejercicio1.Vertex;
+
+
+public class AdjListEdge<T> implements Edge<T> {
+	private Vertex<T> target;
+	private int weight;
+	
+	/**
+	 * Crea una arista.  Se invoca desde AdjListGraph.
+	 */
+	AdjListEdge(Vertex<T> target, int weight){
+		this.target = target;
+		this.weight = weight;
+	}
+	
+	@Override
+	public Vertex<T> getTarget() {
+		return target;
+	}
+
+	@Override
+	public int getWeight() {
+		return weight;
+	}
+}
