50263e4ffee300a18b0ed5e63cb25c181e36f2b1
Matheo Joaquín Lamiral <matheolamiral05@gmail.com>
2025-09-19 15:39:59 -0300

    Práctica 3 y Consignas 1, 2 y 3

---
+++ Tercer_Año/SegundoSemestre/Taller_de_tecnologias_de_produccion_de_software/Practica03/Filtros/src/main/java/org/example/filtros/FiltroLenguajeCliente.java
@@ -0,0 +1,4 @@
+package org.example.filtros;
+
+public class FiltroLenguajeCliente {
+}
---
+++ Tercer_Año/SegundoSemestre/Taller_de_tecnologias_de_produccion_de_software/Practica03/Filtros/src/main/java/org/example/filtros/LoginMultilenguaje.java
@@ -0,0 +1,18 @@
+package org.example.filtros;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "LoginMultilenguaje", value = "/LoginMultilenguaje")
+public class LoginMultilenguaje extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
